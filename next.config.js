d605ddc2dbdad39bd68ca4cb98cc40631b7c9c0f
Andreas Bielawski <brawl@nyanya.de>
2021-11-29 21:33:10 +0100

    Remove swcMinify because of an infinite loop (it's experimental anyway)

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
     // Warning: Only enable this if you do linting through CI before pushing to production!
     ignoreDuringBuilds: true,
   },
-  swcMinify: true,
   async redirects() {
     return [
       {
