7feeb5944d74a94b3fdfa61560c625f85603b3f5
Andreas Bielawski <brawl@nyanya.de>
2021-11-30 16:14:17 +0100

    Reorganize mii checks

--- src/lib/constants/miiType.js
+++ src/lib/constants/miiType.js
@@ -2,3 +2,6 @@ export const MII_TYPE = Object.freeze({
   GUEST: 'guest',
   CMOC: 'cmoc',
 });
+
+export const isValidMiiType = (value) =>
+  [MII_TYPE.GUEST, MII_TYPE.CMOC, MII_TYPE.QR].includes(value);
