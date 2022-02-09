4753b59c810641c9ff0d99851565e86cbba710fc
Ben <bennyman123abc@hotmail.com>
2022-02-09 21:34:45 +0000

    Converted Class Components to Function Components

--- src/components/credits/CreditsMusic.jsx
+++ src/components/credits/CreditsMusic.jsx
@@ -6,90 +6,68 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 
-export default class CreditsMusic extends React.Component {
-    constructor(properties) {
-        super(properties);
-        this.state = {
-            videoId: properties.videoId,
-            playing: true,
-            player: null,
-            hidden: false,
-        }
-    }
+export default function CreditsMusic({ videoId, getMusic }) {
+    const [playing, setPlaying] = React.useState(true);
+    const [player, setPlayer] = React.useState(null);
+    const [video, setVideo] = React.useState(videoId);
+    const [hidden, setHidden] = React.useState(false);
 
-    onClick = async () => {
-        const { player, playing } = this.state;
-
-        this.setState({
-            playing: !playing,
-        });
-        playing ? player.playVideo() : this.pause();
-        if (playing) {
-            toast.success(`Now playing: ${player.getVideoData().title}`);
-        }
-    }
-
-    onReady = (event) => {
-        this.setState({
-            player: event.target,
-        });
-    }
-
-    onError = () => {
-        this.setState({
-            hidden: true,
-        });
-    }
-
-    pause() {
-        const { player } = this.state;
-        const { getMusic } = this.props;
-
+    const pause = () => {
         player.stopVideo();
-        this.setState({
-            videoId: getMusic()
-        });
+        setVideo(getMusic());
     }
 
-    render() {
-        const { videoId, playing, hidden } = this.state;
-        return (
-            <div>
-                <YouTube 
-                    videoId={videoId}
-                    opts={{
-                        height: '0',
-                        width: '0',
-                        playerVars: {
-                            controls: 1,
-                            showinfo: 0,
-                            modestbranding: 1,
-                        },
-                    }}
-                    onReady={this.onReady}
-                    onError={this.onError}
-                />
-                <Button 
-                    variant="success" 
-                    onClick={this.onClick}
-                    className="rounded-circle shadow position-fixed"
-                    style={{
-                        bottom: '7%',
-                        right: '5%',
-                        width: 70,
-                        height: 70,
-                        fontSize: '2rem',
-                        zIndex: 99,
-                        visibility: hidden ? 'hidden' : 'visible',
-                    }}
-                >
-                    {playing ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faStop} />}
-                </Button>
-            </div>
-        )
+    const onReady = (event) => {
+        setPlayer(event.target);
     }
+
+    const onError = () => {
+        setHidden(true);
+    }
+
+    const onClick = async () => {
+        setPlaying(!playing);
+        playing ? player.playVideo() : pause();
+        if (playing) toast.success(`Now playing: ${player.getVideoData().title}`);
+    }
+
+    return (
+        <div>
+            <YouTube 
+                videoId={video}
+                opts={{
+                    height: '0',
+                    width: '0',
+                    playerVars: {
+                        controls: 1,
+                        showinfo: 0,
+                        modestbranding: 1,
+                    },
+                }}
+                onReady={onReady}
+                onError={onError}
+            />
+            <Button 
+                variant="success" 
+                onClick={onClick}
+                className="rounded-circle shadow position-fixed"
+                style={{
+                    bottom: '7%',
+                    right: '5%',
+                    width: 70,
+                    height: 70,
+                    fontSize: '2rem',
+                    zIndex: 99,
+                    visibility: hidden ? 'hidden' : 'visible',
+                }}
+            >
+                {playing ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faStop} />}
+            </Button>
+        </div>
+    );
 }
 
 CreditsMusic.propTypes = {
+    videoId: PropTypes.string.isRequired,
     getMusic: PropTypes.func.isRequired,
-}
+};
