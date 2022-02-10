044e8ef995698f38c5f7b4ea1321d2fae7176285
Andreas Bielawski <brawl@nyanya.de>
2022-02-10 18:34:50 +0100

    Handle rate limit on tag refresh

--- src/components/account/RefreshTagButton.jsx
+++ src/components/account/RefreshTagButton.jsx
@@ -1,11 +1,16 @@
 import { toast } from 'react-toastify';
 import { Button } from 'react-bootstrap';
+import HTTP_CODE from '@/lib/constants/httpStatusCodes';
 
 async function refresh() {
   toast.promise(fetch('/api/account/refresh-tag', { method: 'POST' }), {
     pending: 'Refreshing your tag...',
     success: {
       render({ data, toastProps }) {
+        if (data.status === HTTP_CODE.TOO_MANY_REQUESTS) {
+          toastProps.type = 'error';
+          return 'Woah, please slow down!';
+        }
         if (data.status !== 200) {
           toastProps.type = 'error';
           return 'An error occured, please try again later';
