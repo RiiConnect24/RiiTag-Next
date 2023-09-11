38af47898e060bdf94185ee2cffe20296d630d81
Larsenv <larsen@weborican.com>
2023-09-11 17:09:46 -0500

    Fix downloading switchtdb

--- src/lib/utils/riitagUtils.js
+++ src/lib/utils/riitagUtils.js
@@ -6,61 +6,57 @@ import path from 'path';
 import { DATA } from '@/lib/constants/filePaths';
 
 export async function downloadSwitchTDB(req, res) {
+  const response = await fetch('https://www.gametdb.com/switchtdb.zip');
+
+  if (response.status !== 200) {
+    logger.error(`Failed downloading ${txtname}.txt`);
+    return Promise.reject();
+  }
+
+  // Step 2: Unzip switchtdb.zip and save switchtdb.xml
+  const zip = new AdmZip(response.body);
   try {
-    const response = await fetch('https://www.gametdb.com/switchtdb.zip');
-
-    if (response.status !== 200) {
-      logger.error(`Failed downloading ${txtname}.txt`);
-      return Promise.reject();
-    }
-
-    // Step 2: Unzip switchtdb.zip and save switchtdb.xml
-    const zip = new AdmZip(response.body);
-    try {
-      zip.extractAllTo(path.resolve(DATA.IDS), true);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-
-    // Step 3: Read and modify the XML
-    const xmlData = await fs.readFile(path.resolve(DATA.IDS, 'switchtdb.xml'));
-    const jsonData = await xml2js.parseStringPromise(xmlData, { explicitArray: false });
-
-    // Scrubbing logic (modify jsonData as needed)
-
-    // Step 4: Save the modified XML
-    const builder = new xml2js.Builder();
-    const modifiedXml = builder.buildObject(jsonData);
-    await fs.writeFile(path.resolve(DATA.IDS, 'switchtdb.xml'), modifiedXml);
-
-    // Step 5: Create switchtdb.json
-    const gameData = jsonData.datafile.game;
-    const gameMap = {};
-
-    console.log(gameData);
-
-    gameData.forEach((game) => {
-      const name = game["$"].name.replace(/ \(EN\)$/, '');
-      const id = game.id;
-      if (!gameMap[name]) {
-        gameMap[name] = [];
-      }
-      gameMap[name].push(id);
-    });
-
-    // Step 6: Sort the keys alphabetically
-    const sortedGameMap = {};
-    Object.keys(gameMap)
-      .sort()
-      .forEach((key) => {
-        sortedGameMap[key] = gameMap[key];
-      });
-
-    await fs.writeFile(
-      path.resolve(DATA.IDS, 'switchtdb.json'),
-      JSON.stringify(sortedGameMap, null, 2)
-    );
+    zip.extractAllTo(path.resolve(DATA.IDS), true);
   } catch (error) {
     console.error('Error:', error);
   }
+
+  // Step 3: Read and modify the XML
+  const xmlData = await fs.readFile(path.resolve(DATA.IDS, 'switchtdb.xml'));
+  const jsonData = await xml2js.parseStringPromise(xmlData, { explicitArray: false });
+
+  // Scrubbing logic (modify jsonData as needed)
+
+  // Step 4: Save the modified XML
+  const builder = new xml2js.Builder();
+  const modifiedXml = builder.buildObject(jsonData);
+  await fs.writeFile(path.resolve(DATA.IDS, 'switchtdb.xml'), modifiedXml);
+
+  // Step 5: Create switchtdb.json
+  const gameData = jsonData.datafile.game;
+  const gameMap = {};
+
+  console.log(gameData);
+
+  gameData.forEach((game) => {
+    const name = game["$"].name.replace(/ \(EN\)$/, '');
+    const id = game.id;
+    if (!gameMap[name]) {
+      gameMap[name] = [];
+    }
+    gameMap[name].push(id);
+  });
+
+  // Step 6: Sort the keys alphabetically
+  const sortedGameMap = {};
+  Object.keys(gameMap)
+    .sort()
+    .forEach((key) => {
+      sortedGameMap[key] = gameMap[key];
+    });
+
+  await fs.writeFile(
+    path.resolve(DATA.IDS, 'switchtdb.json'),
+    JSON.stringify(sortedGameMap, null, 2)
+  );
 }
